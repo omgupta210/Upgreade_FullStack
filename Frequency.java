2f9cbc25f104e4aae042e3d7fc9b45fdb01cab06
omgupta210 <omgupta210@gmail.com>
2022-06-02 20:07:32 +0530

    first commit

--- Frequency.java
+++ Frequency.java
@@ -29,11 +29,37 @@ public class Frequency {
 	        return res;
 	        
 	    }
+		public static void sortbyval(Map<Integer,Integer> h)
+		{
+			ArrayList<Map.Entry<Integer,Integer>> li=new ArrayList<>(h.entrySet());
+                  System.out.println(li);
+
+            Collections.sort(li,(Map.Entry<Integer, Integer>o1,Map.Entry<Integer, Integer>o2)->
+				{
+					 if(o1.getValue()>o2.getValue())
+					   return 1;
+					else
+					  return -1;
+				}
+			);
+
+			for(Map.Entry<Integer,Integer> k: li)
+			{
+                  System.out.println(k.getKey()+ " "+k.getValue());
+			}
+		}
 	   public static void main(String[] args)
 	   {
 		   int a[]=new int[]{2,2,1};
 		  int a1= Frequency.singleNumber(a);
 		  //System.out.println(a1);
+		  Map<Integer,Integer> hmap=new HashMap<>();
+		  hmap.put(1,12);
+		  hmap.put(3,7);
+          hmap.put(5,15);
+		  hmap.put(7,1);
+
+		  sortbyval(hmap);
 	   }
 	
 }
